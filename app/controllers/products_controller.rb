73c9aaf4f651ce7e488c1b764a965cdd8f775f61
mindy <mindy22050@gmail.com>
2016-03-17 21:22:30 +0800

    新手村，練習到購物車的商品明細

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,7 +14,7 @@ class ProductsController < ApplicationController
       flash[:notice] ="你已成功將 #{@product.title}加到購物車了"   #Q1  #{}這是什麼意思 A:rails的變數
                                                      #Q2  為什麼不是Product.title A:因為前面已經宣告@product了
     else
-      flash[:notice] ="你的購物車裡已經有此商品"
+      flash[:Warning] ="你的購物車裡已經有此商品"
     end
 
     redirect_to :back
